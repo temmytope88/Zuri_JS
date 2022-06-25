9c2cb028fa10e3859704f46bf7f5e71f65f40962
temmytope88 <fakiletemitope@gmail.com>
2022-06-25 04:45:19 +0100

    Js Intro

--- into.js/intro.js
+++ into.js/intro.js
@@ -1,1 +1,7 @@
-nltrjojsrotoijothgiti
+var name = 'Temitope Fakile';
+var height = 5.9;
+var country = 'Nigeria';
+
+document.write("Name : "+ "<strong>" + name + "</strong>"  + "<br>" 
++ 'Height: ' + "<strong>" + height + " ft" + "</strong>"  + "<br>"
++ "Country: " + "<strong>" + country+ "</strong>" );
